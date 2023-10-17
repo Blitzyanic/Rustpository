606e8858dbaa1bd324eff1d193d924a49351cdc6
Yanic <yanic.dev@gmail.com>
2023-10-17 22:54:40 +0200

    basic struct with rectangle example

--- src/structs.rs
+++ src/structs.rs
@@ -1,2 +1,14 @@
+struct Rectangle {
+    width: u16,
+    height: u16
+}
+
 pub fn structs() {
+    let rect1: Rectangle = Rectangle { width: 30, height: 50 };
+
+    println!("{}", calc(&rect1));
+}
+
+fn calc(rect: &Rectangle) -> u16 {
+    rect.width * rect.height
 }
