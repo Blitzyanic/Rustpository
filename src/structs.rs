ebef90e5ede353d438e6014a417b8ba72435e343
Yanic <yanic.dev@gmail.com>
2023-10-20 16:37:16 +0200

    add methods & finish struct

--- src/structs.rs
+++ src/structs.rs
@@ -2,13 +2,14 @@ struct Rectangle {
     width: u16,
     height: u16
 }
+impl Rectangle {
+    fn calc(&self, rect: &Rectangle) -> u16 {
+        rect.width * rect.height
+    }
+}
 
 pub fn structs() {
     let rect1: Rectangle = Rectangle { width: 30, height: 50 };
 
-    println!("{}", calc(&rect1));
-}
-
-fn calc(rect: &Rectangle) -> u16 {
-    rect.width * rect.height
+    println!("{}", rect1.calc(&rect1));
 }
